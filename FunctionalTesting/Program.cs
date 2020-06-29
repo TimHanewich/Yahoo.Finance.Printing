6561e26d1984bd0c74f54c384f6697a0b7ee5f85
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-06-28 21:15:06 -0400

    Initial commit

---
+++ FunctionalTesting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Yahoo.Finance;
+using Yahoo.Finance.Printing;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace FunctionalTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string take_from = "C:\\Users\\tihanewi\\Downloads\\06282020";
+            string contenttt = System.IO.File.ReadAllText(take_from);
+            Equity[] allequities = JsonConvert.DeserializeObject<Equity[]>(contenttt);
+
+
+            string content = allequities.PrintToCsvContent();
+
+            string path = "C:\\Users\\tihanewi\\Downloads\\WriteHere.csv";
+            System.IO.File.WriteAllText(path, content);
+            Console.WriteLine("Done");
+
+            
+        }
+    }
+}
